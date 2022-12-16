f6f65b74aef6af46abf90ddde5197a7382fada53
Villelmo <willitc9888@gmail.com>
2022-12-16 14:06:51 -0600

    add values input and output

---
+++ calculator.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use diagnostics;
+
+# declare variables 
+my $var1 = 2;
+my $var2 = 3;
+
+
+# output of the program 
+my $result = $var1 * $var2;
+
+
+print $result;
+
+
